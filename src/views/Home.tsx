0b5f380486e4e969be3d879c93d7ce9082857526
zhangchao <544262408@qq.com>
2023-05-10 21:49:46 +0800

    test

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,20 +1,91 @@
 import React from 'react'
 import Header from '../components/Header'
+import iconmemory from '../images/icon-memory.svg'
+import iconreaction from '../images/icon-reaction.svg'
+import iconverbal from '../images/icon-verbal.svg'
+import iconvisual from '../images/icon-visual.svg'
 
+// https://www.youtube.com/watch?v=KqFAs5d3Yl8&t=797s
 const Home = () => {
   return (
     <React.Fragment>
-      <Header title="Home" />
       <main>
-        <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
-          {/* Replace with your content */}
-          <div className="px-4 py-6 sm:px-0">
-            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96 p-4 text-center text-gray-400">
-              Here goes your content. You can also go the About page.
+        <section className=" container m-auto  font-Hanken">
+          <div className='flex flex-row h-screen  items-start sm:items-center   justify-center'>
+
+          
+          <div className=" flex flex-col xl:flex-row  rounded-3xl  w-ful sm:max-w-3xl sm:m-auto shadow-xl shadow-Neutral-Paleblue">
+            <div className='  bg-Primary-CobaltBlue-600 w-full  sm:w-1/2 text-center  text-Neutral-Lightlavender rounded-b-3xl  sm:rounded-3xl '>
+              <div className='p-8'>Your Result</div>
+              <div className=' flex flex-col   space-y-4 '>
+                <div className=' flex flex-col  items-center  justify-center  w-32 h-32  sm:w-52 sm:h-52 rounded-full bg-gradient-to-r  from-Gradients-Lightroyalblue to-Gradients-Violetblue  m-auto'>
+                  <div className=' text-4xl  sm:text-8xl font-bold text-Neutral-White'>76</div>
+                  <div>of 100</div>
+                </div>
+                
+                <div className=' font-semibold text-2xl text-Neutral-White'>Great</div>
+                <div className='mx-8  pb-8'>You scored higher than 65% of the people who have taken these tests.</div>
+              </div>
+              
+              
             </div>
+            <div className='sm:w-1/2 rounded-3xl    '>
+              <h1 className='  p-8 font-semibold'>Summay</h1>
+
+              <div className=' flex flex-row   mx-8 my-4 mt-0  bg-Primary-Lightred-50 p-4 rounded-lg'>
+                <div className=' w-10'>
+                  <img src={iconreaction}></img>
+                </div>
+                <div className=' flex-1'>
+                Reaction
+                </div>
+                <div className=' w-1/5'>
+                  50/100
+                </div>
+              </div>
+              <div className=' flex flex-row mx-8 my-4  bg-Primary-OrangeyYellow-50 p-4 rounded-lg'>
+              <div className=' w-10'>
+              <img src={iconmemory}></img>
+                </div>
+                <div className=' flex-1'>
+                Reaction
+                </div>
+                <div className=' w-1/5'>
+                  50/100
+                </div>
+              </div>
+              <div className=' flex flex-row mx-8 my-4  bg-Primary-GreenTeal-50 p-4 rounded-lg'>
+              <div className=' w-10'>
+              <img src={iconverbal}></img>
+                </div>
+                <div className=' flex-1'>
+                Reaction
+                </div>
+                <div className=' w-1/5'>
+                  50/100
+                </div>
+              </div>
+              <div className=' flex flex-row mx-8 my-4   bg-Primary-CobaltBlue-50 p-4 rounded-lg'>
+              <div className=' w-10'>
+              <img src={iconvisual}></img>
+                </div>
+                <div className=' flex-1'>
+                Reaction
+                </div>
+                <div className=' w-1/5'>
+                  50/100
+                </div>
+              </div>
+              <div className='flex  flex-row p-8'>
+              <button className=' w-full  m-auto bg-Neutral-Darkgrayblue  text-Neutral-White  rounded-2xl  px-8  py-3 cursor-pointer leading-none hover:bg-Primary-CobaltBlue-600'> Continue</button>
+              </div>
+              
+            </div>
+
           </div>
-          {/* /End replace */}
-        </div>
+          </div>
+        </section>
+      
       </main>
     </React.Fragment>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,8 +8,84 @@ module.exports = {
       fontWeight: ['hover', 'focus'],
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        Hanken: ['Hanken','Grotesk', 'sans-serif'],
+      
       },
     },
+    screens: {
+      sm: '375px',
+      xl: '1440px',
+    },
+    colors: {
+      Primary:{
+        Lightred: {
+          '50': '#fff1f1',
+          '100': '#ffe1e1',
+          '200': '#ffc7c7',
+          '300': '#ffa0a0',
+          '400': '#ff5757',
+          '500': '#f83b3b',
+          '600': '#e51d1d',
+          '700': '#c11414',
+          '800': '#a01414',
+          '900': '#841818',
+          '950': '#480707',
+        },
+        OrangeyYellow: {
+          '50': '#fffaeb',
+          '100': '#fff0c6',
+          '200': '#ffde88',
+          '300': '#ffc84a',
+          '400': '#ffb01f',
+          '500': '#f98d07',
+          '600': '#dd6702',
+          '700': '#b74606',
+          '800': '#94350c',
+          '900': '#7a2c0d',
+          '950': '#461502',
+        },
+        GreenTeal: {
+          '50': '#eafff7',
+          '100': '#cdfeea',
+          '200': '#a0fada',
+          '300': '#63f2c7',
+          '400': '#25e2b0',
+          '500': '#00bd91',
+          '600': '#00a47e',
+          '700': '#008369',
+          '800': '#006754',
+          '900': '#005546',
+          '950': '#003029',
+        },
+        CobaltBlue: {
+          '50': '#f2f6ff',
+          '100': '#e7edff',
+          '200': '#d3dfff',
+          '300': '#afc2ff',
+          '400': '#829bff',
+          '500': '#4f68ff',
+          '600': '#2b3dfc',
+          '700': '#1926e8',
+          '800': '#1622d0',
+          '900': '#131c9f',
+          '950': '#09136c',
+        }
+      },
+      Gradients:{
+        Lightslateblue:"hsl(252, 100%, 67%)",
+        Lightroyalblue :"hsl(241, 81%, 54%)",
+        Violetblue :"hsla(256, 72%, 46%, 1)",
+        Persianblue:"hsla(241, 72%, 46%, 0)",
+      },
+      Neutral:{
+        White:"hsl(0, 0%, 100%)",
+        Paleblue:"hsl(221, 100%, 96%)",
+        Lightlavender:"hsl(241, 100%, 89%)",
+        Darkgrayblue:"hsl(224, 30%, 27%)"
+      }
+    },
+    
+
   },
   variants: {},
   plugins: []
